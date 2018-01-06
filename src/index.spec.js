dd332f2b15ec58cbdb79bc842c2b59fc7600bbee
Fabio Elizandro <fabio@elizandro.com.br>
2018-01-07 03:19:42 +1100

    Change index spec.

--- src/index.spec.js
+++ src/index.spec.js
@@ -1,22 +1,16 @@
-const { createCommandHandler, createCommandMap } = require('./index');
+const facade = require('./index');
+const parallelizeCommandHandlers = require('./parallelize-command-handlers');
+const commandCreator = require('./command-creator');
+const createCommandMap = require('./create-command-map');
+const createCommandHandler = require('./create-command-handler');
 
-describe('create command handler', () => {
-  it('creates the command handler', async () => {
-    const commandHandler1 = jest.fn();
-    const commandHandler2 = jest.fn();
-    const eventDispatcher = jest.fn();
-    const command = { type: 'DO_SOMETHING_1' };
-
-    const MAP = {
-      'DO_SOMETHING_1': [commandHandler1],
-      'DO_SOMETHING_2': [commandHandler2]
-    };
-
-    const commandHandler = createCommandHandler({ commandMap: createCommandMap(MAP), eventDispatcher });
-
-    await commandHandler(command);
-
-    expect(commandHandler1).toHaveBeenCalledWith({ command, eventDispatcher });
-    expect(commandHandler2).not.toHaveBeenCalled();
+describe('library facade', () => {
+  it('exports the functions', () => {
+    expect(facade).toEqual({
+      parallelizeCommandHandlers,
+      commandCreator,
+      createCommandMap,
+      createCommandHandler
+    });
   });
 });
